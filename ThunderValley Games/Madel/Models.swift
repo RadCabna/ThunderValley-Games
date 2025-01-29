198130408fe98d54299a7ef349c1c5c8fd7d1804
Radcabna <radcabna@gmail.com>
2025-01-30 01:18:53 +0300

    Correct line display

--- ThunderValley Games/Madel/Models.swift
+++ ThunderValley Games/Madel/Models.swift
@@ -11,9 +11,8 @@ struct Thunder: Codable, Equatable {
     var itemName: String = ""
     var positionX: CGFloat
     var positionY: CGFloat
-    var offsetX: CGFloat = 0
-    var offsetY: CGFloat = 0
-    var opacity: CGFloat = 1
+    var haveThunder = false
+    var yourThunder = true
 }
 
 struct Lines: Codable, Equatable {
@@ -40,8 +39,10 @@ class Arrays {
         [Thunder(positionX: -128, positionY: -128), Thunder(positionX: 0, positionY: -128), Thunder(positionX: 128, positionY: -128)],
         [Thunder(positionX: -97, positionY: -97), Thunder(positionX: 0, positionY: -97), Thunder(positionX: 97, positionY: -97)],
         [Thunder(positionX: -67, positionY: -67), Thunder(positionX: 0, positionY: -67), Thunder(positionX: 67, positionY: -67)],
+        
         [Thunder(positionX: -128, positionY: -2), Thunder(positionX: -97, positionY: -2), Thunder(positionX: -67, positionY: -2)],
         [Thunder(positionX: 67, positionY: -2), Thunder(positionX: 97, positionY: -2), Thunder(positionX: 128, positionY: -2)],
+        
         [Thunder(positionX: -67, positionY: 67), Thunder(positionX: 0, positionY: 67), Thunder(positionX: 67, positionY: 67)],
         [Thunder(positionX: -97, positionY: 97), Thunder(positionX: 0, positionY: 97), Thunder(positionX: 97, positionY: 97)],
         [Thunder(positionX: -128, positionY: 128), Thunder(positionX: 0, positionY: 128), Thunder(positionX: 128, positionY: 128)]
@@ -54,5 +55,14 @@ class Arrays {
     [Lines(itemName: "blueHorizontal1", positionX: 0, positionY: -128), Lines(itemName: "blueHorizontal2", positionX: 0, positionY: -97), Lines(itemName: "blueHorizontal3", positionX: 0, positionY: -67)],
     [Lines(itemName: "blueHorizontal1", positionX: 0, positionY: 128), Lines(itemName: "blueHorizontal2", positionX: 0, positionY: 97), Lines(itemName: "blueHorizontal3", positionX: 0, positionY: 67)],
     [Lines(itemName: "blueHorizontal4", positionX: -97, positionY: 0), Lines(itemName: "blueHorizontal4", positionX: 97, positionY: 0)]
+    ]
+    
+    static var redLinesOnGameField: [[Lines]] = [
+    [Lines(itemName: "redVertical1", positionX: -128, positionY: 0), Lines(itemName: "redVertical2", positionX: -97, positionY: 0), Lines(itemName: "redVertical3", positionX: -67, positionY: 0),],
+    [Lines(itemName: "redVertical1", positionX: 128, positionY: 0), Lines(itemName: "redVertical2", positionX:97, positionY: 0), Lines(itemName: "redVertical3", positionX: 67, positionY: 0)],
+    [Lines(itemName: "redVertical4", positionX: 0, positionY: -97), Lines(itemName: "redVertical4", positionX: 0, positionY: 97)],
+    [Lines(itemName: "redHorizontal1", positionX: 0, positionY: -128), Lines(itemName: "redHorizontal2", positionX: 0, positionY: -97), Lines(itemName: "redHorizontal3", positionX: 0, positionY: -67)],
+    [Lines(itemName: "redHorizontal1", positionX: 0, positionY: 128), Lines(itemName: "redHorizontal2", positionX: 0, positionY: 97), Lines(itemName: "redHorizontal3", positionX: 0, positionY: 67)],
+    [Lines(itemName: "redHorizontal4", positionX: -97, positionY: 0), Lines(itemName: "redHorizontal4", positionX: 97, positionY: 0)]
     ]
 }
--- ThunderValley Games/Views/Game.swift
+++ ThunderValley Games/Views/Game.swift
@@ -14,6 +14,7 @@ struct Game: View {
     @State private var translationY: CGFloat = 0
     @State private var rectanglesOnGameField = Arrays.rectanglesOnGameField
     @State private var linesOnGameField = Arrays.blueLinesOnGameField
+    @State private var enemyLinesOnGameField = Arrays.redLinesOnGameField
     @State private var rectangleLineWidth: CGFloat = 0
     @State private var yourTurn = true
     @State private var yourThunderCount = 9
@@ -95,11 +96,24 @@ struct Game: View {
                                     }
                                 }
                             }
-                            
                             ZStack {
+                                ForEach(0..<enemyLinesOnGameField.count, id: \.self) { row in
+                                    ForEach(0..<enemyLinesOnGameField[row].count, id: \.self) { col in
+                                        if enemyLinesOnGameField[row][col].lineActive {
+                                            Image(enemyLinesOnGameField[row][col].itemName)
+                                                .scaleEffect(x: screenWidth/932, y: screenWidth/932)
+                                                .offset(
+                                                    x: enemyLinesOnGameField[row][col].positionX * screenWidth/932,
+                                                    y: enemyLinesOnGameField[row][col].positionY * screenWidth/932
+                                                )
+                                        }
+                                    }
+                                }
+                            }
+                            
                                 ForEach(0..<rectanglesOnGameField.count, id: \.self) { row in
                                     ForEach(0..<rectanglesOnGameField[row].count, id: \.self) { col in
-                                        
+                                        ZStack {
                                         RoundedRectangle(cornerRadius: 1.0)
                                             .frame(width: width*0.025, height: width*0.025)
                                             .foregroundColor(.black)
@@ -109,9 +123,20 @@ struct Game: View {
                                             .frame(width: width*0.025, height: width*0.025)
                                             .foregroundColor(.white)
                                             .offset(x: rectanglesOnGameField[row][col].positionX * screenWidth/932, y: rectanglesOnGameField[row][col].positionY * screenWidth/932)
-                                        
-                                        
+                                        if rectanglesOnGameField[row][col].haveThunder {
+                                            Image(rectanglesOnGameField[row][col].yourThunder ? "blueThunder" : "redThunder")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: width*0.02)
+                                                .offset(x: rectanglesOnGameField[row][col].positionX * screenWidth/932, y: rectanglesOnGameField[row][col].positionY * screenWidth/932)
+                                        }
                                     }
+                                        .onTapGesture {
+                                            rectanglesOnGameField[row][col].haveThunder.toggle()
+                                            rectanglesOnGameField[row][col].yourThunder = yourTurn
+                                           checkLines()
+                                            yourTurn.toggle()
+                                        }
                                 }
                             }
                         }
@@ -133,6 +158,297 @@ struct Game: View {
             AppDelegate().setOrientation(to: .landscapeLeft)
             print("width: \(screenWidth)")
             print("height: \(screenHeight)")
+        }
+    }
+    
+    func checkLines() {
+        checkLeftVerticalLine1()
+        checkLeftVerticalLine2()
+        checkLeftVerticalLine3()
+        checkRightVerticalLine1()
+        checkRightVerticalLine2()
+        checkRightVerticalLine3()
+        checkMiddleTopLine()
+        checkMiddleBottomLine()
+        checkTopHorizontalLine1()
+        checkTopHorizontalLine2()
+        checkTopHorizontalLine3()
+        checkBottomHorizontalLine1()
+        checkBottomHorizontalLine2()
+        checkBottomHorizontalLine3()
+        checkMiddleLeftLine()
+        checkMiddleRightLine()
+    }
+    
+    func checkLeftVerticalLine1() {
+        if rectanglesOnGameField[0][0].haveThunder && 
+            rectanglesOnGameField[3][0].haveThunder &&
+            rectanglesOnGameField[7][0].haveThunder &&
+            rectanglesOnGameField[0][0].yourThunder == rectanglesOnGameField[3][0].yourThunder &&
+            rectanglesOnGameField[0][0].yourThunder == rectanglesOnGameField[7][0].yourThunder {
+            if rectanglesOnGameField[0][0].yourThunder {
+                linesOnGameField[0][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[0][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[0][0].lineActive = false
+            enemyLinesOnGameField[0][0].lineActive = false
+        }
+    }
+    
+    func checkLeftVerticalLine2() {
+        if rectanglesOnGameField[1][0].haveThunder &&
+            rectanglesOnGameField[3][1].haveThunder &&
+            rectanglesOnGameField[6][0].haveThunder &&
+            rectanglesOnGameField[1][0].yourThunder == rectanglesOnGameField[3][1].yourThunder &&
+            rectanglesOnGameField[1][0].yourThunder == rectanglesOnGameField[6][0].yourThunder {
+            if rectanglesOnGameField[1][0].yourThunder {
+                linesOnGameField[0][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[0][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[0][1].lineActive = false
+            enemyLinesOnGameField[0][1].lineActive = false
+        }
+    }
+    
+    func checkLeftVerticalLine3() {
+        if rectanglesOnGameField[2][0].haveThunder &&
+            rectanglesOnGameField[3][2].haveThunder &&
+            rectanglesOnGameField[5][0].haveThunder &&
+            rectanglesOnGameField[2][0].yourThunder == rectanglesOnGameField[3][2].yourThunder &&
+            rectanglesOnGameField[2][0].yourThunder == rectanglesOnGameField[5][0].yourThunder {
+            if rectanglesOnGameField[2][0].yourThunder {
+                linesOnGameField[0][2].lineActive = true
+            } else {
+                enemyLinesOnGameField[0][2].lineActive = true
+            }
+        } else {
+            linesOnGameField[0][2].lineActive = false
+            enemyLinesOnGameField[0][2].lineActive = false
+        }
+    }
+    
+    func checkRightVerticalLine1() {
+        if rectanglesOnGameField[2][2].haveThunder &&
+            rectanglesOnGameField[4][0].haveThunder &&
+            rectanglesOnGameField[5][2].haveThunder &&
+            rectanglesOnGameField[2][2].yourThunder == rectanglesOnGameField[4][0].yourThunder &&
+            rectanglesOnGameField[2][2].yourThunder == rectanglesOnGameField[5][2].yourThunder {
+            if rectanglesOnGameField[2][2].yourThunder {
+                linesOnGameField[1][2].lineActive = true
+            } else {
+                enemyLinesOnGameField[1][2].lineActive = true
+            }
+        } else {
+            linesOnGameField[1][2].lineActive = false
+            enemyLinesOnGameField[1][2].lineActive = false
+        }
+    }
+    
+    func checkRightVerticalLine2() {
+        if rectanglesOnGameField[1][2].haveThunder &&
+            rectanglesOnGameField[4][1].haveThunder &&
+            rectanglesOnGameField[6][2].haveThunder &&
+            rectanglesOnGameField[1][2].yourThunder == rectanglesOnGameField[4][1].yourThunder &&
+            rectanglesOnGameField[1][2].yourThunder == rectanglesOnGameField[6][2].yourThunder {
+            if rectanglesOnGameField[1][2].yourThunder {
+                linesOnGameField[1][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[1][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[1][1].lineActive = false
+            enemyLinesOnGameField[1][1].lineActive = false
+        }
+    }
+    
+    func checkRightVerticalLine3() {
+        if rectanglesOnGameField[0][2].haveThunder &&
+            rectanglesOnGameField[4][2].haveThunder &&
+            rectanglesOnGameField[7][2].haveThunder &&
+            rectanglesOnGameField[0][2].yourThunder == rectanglesOnGameField[4][2].yourThunder &&
+            rectanglesOnGameField[0][2].yourThunder == rectanglesOnGameField[7][2].yourThunder {
+            if rectanglesOnGameField[0][2].yourThunder {
+                linesOnGameField[1][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[1][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[1][0].lineActive = false
+            enemyLinesOnGameField[1][0].lineActive = false
+        }
+    }
+    
+    func checkMiddleTopLine() {
+        if rectanglesOnGameField[0][1].haveThunder &&
+            rectanglesOnGameField[1][1].haveThunder &&
+            rectanglesOnGameField[2][1].haveThunder &&
+            rectanglesOnGameField[0][1].yourThunder == rectanglesOnGameField[1][1].yourThunder &&
+            rectanglesOnGameField[0][1].yourThunder == rectanglesOnGameField[2][1].yourThunder {
+            if rectanglesOnGameField[0][1].yourThunder {
+                linesOnGameField[2][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[2][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[2][0].lineActive = false
+            enemyLinesOnGameField[2][0].lineActive = false
+        }
+    }
+    
+    func checkMiddleBottomLine() {
+        if rectanglesOnGameField[5][1].haveThunder &&
+            rectanglesOnGameField[6][1].haveThunder &&
+            rectanglesOnGameField[7][1].haveThunder &&
+            rectanglesOnGameField[5][1].yourThunder == rectanglesOnGameField[6][1].yourThunder &&
+            rectanglesOnGameField[5][1].yourThunder == rectanglesOnGameField[7][1].yourThunder {
+            if rectanglesOnGameField[5][1].yourThunder {
+                linesOnGameField[2][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[2][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[2][1].lineActive = false
+            enemyLinesOnGameField[2][1].lineActive = false
+        }
+    }
+    
+    func checkTopHorizontalLine1() {
+        if rectanglesOnGameField[0][0].haveThunder &&
+            rectanglesOnGameField[0][1].haveThunder &&
+            rectanglesOnGameField[0][2].haveThunder &&
+            rectanglesOnGameField[0][0].yourThunder == rectanglesOnGameField[0][1].yourThunder &&
+            rectanglesOnGameField[0][0].yourThunder == rectanglesOnGameField[0][2].yourThunder {
+            if rectanglesOnGameField[0][0].yourThunder {
+                linesOnGameField[3][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[3][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[3][0].lineActive = false
+            enemyLinesOnGameField[3][0].lineActive = false
+        }
+    }
+    
+    func checkTopHorizontalLine2() {
+        if rectanglesOnGameField[1][0].haveThunder &&
+            rectanglesOnGameField[1][1].haveThunder &&
+            rectanglesOnGameField[1][2].haveThunder &&
+            rectanglesOnGameField[1][0].yourThunder == rectanglesOnGameField[1][1].yourThunder &&
+            rectanglesOnGameField[1][0].yourThunder == rectanglesOnGameField[1][2].yourThunder {
+            if rectanglesOnGameField[1][0].yourThunder {
+                linesOnGameField[3][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[3][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[3][1].lineActive = false
+            enemyLinesOnGameField[3][1].lineActive = false
+        }
+    }
+    
+    func checkTopHorizontalLine3() {
+        if rectanglesOnGameField[2][0].haveThunder &&
+            rectanglesOnGameField[2][1].haveThunder &&
+            rectanglesOnGameField[2][2].haveThunder &&
+            rectanglesOnGameField[2][0].yourThunder == rectanglesOnGameField[2][1].yourThunder &&
+            rectanglesOnGameField[2][0].yourThunder == rectanglesOnGameField[2][2].yourThunder {
+            if rectanglesOnGameField[2][0].yourThunder {
+                linesOnGameField[3][2].lineActive = true
+            } else {
+                enemyLinesOnGameField[3][2].lineActive = true
+            }
+        } else {
+            linesOnGameField[3][2].lineActive = false
+            enemyLinesOnGameField[3][2].lineActive = false
+        }
+    }
+    
+    func checkBottomHorizontalLine1() {
+        if rectanglesOnGameField[5][0].haveThunder &&
+            rectanglesOnGameField[5][1].haveThunder &&
+            rectanglesOnGameField[5][2].haveThunder &&
+            rectanglesOnGameField[5][0].yourThunder == rectanglesOnGameField[5][1].yourThunder &&
+            rectanglesOnGameField[5][0].yourThunder == rectanglesOnGameField[5][2].yourThunder {
+            if rectanglesOnGameField[5][0].yourThunder {
+                linesOnGameField[4][2].lineActive = true
+            } else {
+                enemyLinesOnGameField[4][2].lineActive = true
+            }
+        } else {
+            linesOnGameField[4][2].lineActive = false
+            enemyLinesOnGameField[4][2].lineActive = false
+        }
+    }
+    
+    func checkBottomHorizontalLine2() {
+        if rectanglesOnGameField[6][0].haveThunder &&
+            rectanglesOnGameField[6][1].haveThunder &&
+            rectanglesOnGameField[6][2].haveThunder &&
+            rectanglesOnGameField[6][0].yourThunder == rectanglesOnGameField[6][1].yourThunder &&
+            rectanglesOnGameField[6][0].yourThunder == rectanglesOnGameField[6][2].yourThunder {
+            if rectanglesOnGameField[6][0].yourThunder {
+                linesOnGameField[4][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[4][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[4][1].lineActive = false
+            enemyLinesOnGameField[4][1].lineActive = false
+        }
+    }
+    
+    func checkBottomHorizontalLine3() {
+        if rectanglesOnGameField[7][0].haveThunder &&
+            rectanglesOnGameField[7][1].haveThunder &&
+            rectanglesOnGameField[7][2].haveThunder &&
+            rectanglesOnGameField[7][0].yourThunder == rectanglesOnGameField[7][1].yourThunder &&
+            rectanglesOnGameField[7][0].yourThunder == rectanglesOnGameField[7][2].yourThunder {
+            if rectanglesOnGameField[7][0].yourThunder {
+                linesOnGameField[4][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[4][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[4][0].lineActive = false
+            enemyLinesOnGameField[4][0].lineActive = false
+        }
+    }
+    
+    func checkMiddleLeftLine() {
+        if rectanglesOnGameField[3][0].haveThunder &&
+            rectanglesOnGameField[3][1].haveThunder &&
+            rectanglesOnGameField[3][2].haveThunder &&
+            rectanglesOnGameField[3][0].yourThunder == rectanglesOnGameField[3][1].yourThunder &&
+            rectanglesOnGameField[3][0].yourThunder == rectanglesOnGameField[3][2].yourThunder {
+            if rectanglesOnGameField[3][0].yourThunder {
+                linesOnGameField[5][0].lineActive = true
+            } else {
+                enemyLinesOnGameField[5][0].lineActive = true
+            }
+        } else {
+            linesOnGameField[5][0].lineActive = false
+            enemyLinesOnGameField[5][0].lineActive = false
+        }
+    }
+    
+    func checkMiddleRightLine() {
+        if rectanglesOnGameField[4][0].haveThunder &&
+            rectanglesOnGameField[4][1].haveThunder &&
+            rectanglesOnGameField[4][2].haveThunder &&
+            rectanglesOnGameField[4][0].yourThunder == rectanglesOnGameField[4][1].yourThunder &&
+            rectanglesOnGameField[4][0].yourThunder == rectanglesOnGameField[4][2].yourThunder {
+            if rectanglesOnGameField[4][0].yourThunder {
+                linesOnGameField[5][1].lineActive = true
+            } else {
+                enemyLinesOnGameField[5][1].lineActive = true
+            }
+        } else {
+            linesOnGameField[5][1].lineActive = false
+            enemyLinesOnGameField[5][1].lineActive = false
         }
     }
 }
