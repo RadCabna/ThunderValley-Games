bef032c65e8bd427b11713bac262b954a16ef392
Radcabna <radcabna@gmail.com>
2025-01-29 13:14:38 +0300

    Initial Commit

---
+++ ThunderValley Games/ThunderValley_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderValley_GamesApp.swift
+//  ThunderValley Games
+//
+//  Created by Алкександр Степанов on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThunderValley_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
